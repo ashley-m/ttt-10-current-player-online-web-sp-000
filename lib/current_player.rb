49a30d85a8c40ceda8401eaa9f3459cc4fdb00df
Ashley Miller <asm2fe@virginia.edu>
2019-03-11 22:56:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,7 +13,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board).odd?)
+  if (turn_count(board)==1)
+    return "X"
+  elsif (turn_count(board).odd?)
     return "O"
   else
     return "X"
