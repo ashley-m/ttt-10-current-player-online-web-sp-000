9049f31034c7404e12762446c88aab60acc38a4a
Ashley Miller <asm2fe@virginia.edu>
2019-03-11 22:07:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,15 @@
 def turn_count(board)
-  count=0
+  count=1
   board.each |space|
   do
     if space != " "
       count += 1
     end
   end
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "O"
+  
 end
